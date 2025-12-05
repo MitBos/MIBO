1d4d169236a4dc9a050141aa1b8f9dcd38afeab7
MitBos <110454647+MitBos@users.noreply.github.com>
2025-12-05 10:56:06 +0100

    Add web host entry point

---
+++ src/MIBO.Web/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MIBO.Web.Components;
+using MIBO.Web.Endpoints;
+using MIBO.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMiboWebApp();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapIntegrationApiEndpoints();
+
+app.Run();
